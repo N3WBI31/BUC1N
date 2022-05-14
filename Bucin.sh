3f8c409e66ffada58c5e016595e0dfed7d30df17
RelGanz <105323573+N3WBI31@users.noreply.github.com>
2022-05-14 23:40:43 +0700

    Add files via upload

---
+++ Bucin.sh
@@ -0,0 +1,273 @@
+#Coded by RelGanz
+#Jangan di recode bangsat!!
+#Mau recode izin ke nomor 0882-1098-2908
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+clear
+echo $indigo"==================================================================="
+                    toilet -f future Tools Bucin | lolcat
+                    figlet -f future Kumpulan kata-kata | lolcat
+echo $indigo"*"     $yellow"Author  : RelGanz"
+echo $indigo"*"     $yellow"Team    : Bekasi Cyber Team"
+echo $indigo"*"     $yellow"Contact : 0882-1098-2908"
+echo $indigo"==================================================================="
+echo
+echo $cyan "1. Bucin by. RelGanz1"
+echo $cyan "2. Bucin by. Danz"
+echo $green "3. Bucin by. Kim"
+echo $green "4. Bucin by. Mr.kiplix"
+echo $indigo "5. Bucin by. Mr.k0nsl3t"
+echo $indigi " 6. Bucin by. Team Ganz1"
+echo $red "7. Bucin by. LiaCans"
+echo $red "8. Bucin by. RelGanz2"
+echo $yellow "9. Bucin by. Team Ganz2"
+echo $yellow "10. Bucin by. Team Ganz3"
+echo
+echo $indigo"╭─Pilih Mau Bucin Siapa?? "
+read -p "╰─•>  " pil
+
+if [ $pil = '1' ]
+then
+echo
+echo
+clear
+echo
+toilet -f future Bucin by RelGanz | lolcat
+echo
+echo
+echo
+sleep 0.1
+echo $green"jujur, berat rasanya menahan kerinduan ini.."
+sleep 2
+echo $red"sungguh menjengkelkan, tapi aku berusaha bersabar :) "
+sleep 2
+echo $green"menahan rinduku padamu"
+sleep 2
+echo $red"karena hanya sabar dan.."
+sleep 2
+echo $green"mendoakan yang terbaik.."
+sleep 2
+echo $red"untuk kamu disana"
+sleep 2
+echo $green"jika kamu rindu, sabar ya.."
+sleep 2
+echo $red"AKU DISINI JUGA MERINDUKANMU... :')"
+sleep 3
+clear
+echo
+figlet -f future Bucin by RelGanz | lolcat
+echo
+echo
+echo
+sleep 0.1
+echo $red"Baper?"
+echo
+echo
+sleep 2
+echo $cyan"Jadi orang kok baperan banget??"
+sleep 2
+echo $red"ya iyalah.. namanya juga manusia!"
+sleep 2
+echo $cyan"punya otak, punya hati."
+sleep 1
+echo $red"beda sama lu yg ngomongnya suka ceplas-ceplos!"
+sleep 2
+echo $cyan"hati mati, otak gak berfungsi"
+sleep 2
+echo $red"haha.."
+sleep 2
+echo $cyan"ya.. lain kali bisalah.."
+sleep 2
+echo $red"ngomong. tapi mikir.. :)"
+sleep 3
+fi
+
+if [ $pil = '2' ]
+then
+echo
+echo
+clear
+echo
+toilet -f future Bucin Danz | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Jika wanita baik, hanya untuk pria baik.."
+sleep 2
+echo $red"maka bolehkah aku meminta dirimu.."
+sleep 2
+echo $green"untuk merubahku menjadi lebih baik?"
+sleep 3
+fi
+
+if [ $pil = '3' ]
+then
+clear
+echo
+toilet -f future Bucin By Kim | lolcat
+echo
+echo
+sleep 0.1
+echo $purple"Biarpun kamu disana bersamanya.."
+sleep 2
+echo $green"aku tetap menunggumu sampai.."
+sleep 2
+echo $purple"kau bisa memahami perasaanku.. :')"
+sleep 2
+echo $cyan"yang hampa ini kepadamu.."
+sleep 2
+echo $purple"biarpun sayang tetapi kita...."
+echo
+echo
+sleep 3
+figlet -f future Cuma Temenan | lolcat
+sleep 3
+fi
+
+if [ $pil = '4' ]
+then
+clear
+echo
+toilet -f future Bucin by Mr.kiplix | lolcat
+echo
+echo
+sleep 0.1
+echo $indigo"Bukan kamu yg terlalu sibuk cuma aku aja yg berharap lebih ke kamu"
+echo
+echo
+sleep 10
+echo $red"ku tidak pingin kamu pergi meninggalkan ku sendiri berbarengan bayangmu memanglah kita tlah jauh rasanya memanglah kita telah tidak bersama bila memanglah kita ditakdirkan tuk berbarengan selamanya cinta takkan kemana.."
+echo
+echo
+sleep 10
+echo $indigo"Aku mungkin sering membuatmu kesal, tapi percayalah aku tak akan membuatmu menyesal telah bersama ku , Good Morning"
+echo
+echo
+sleep 10
+echo $red"Setiap pertemuan pasti ada perpisahan,namun entah kenapa perpisahan selalu saja terasa berat,apalagi untuk seseorang yang selalu ada dalam hidup ini.."
+sleep 3
+fi
+
+if [ $pil = '5' ]
+then
+clear
+echo
+toilet -f future Bucin by Mr.k0nsl3t | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Terkadang aku bodoh seperti nobita Memperjuangkan seseorang yang dia tidak  tahu hatinya untuk siapa"
+sleep 3
+fi
+
+if [ $pil = '6' ]
+then
+clear
+echo
+toilet -f future Team Ganz1 | lolcat
+echo
+echo
+sleep 0.1
+echo $white"Hargai disaat aku bertahan"
+sleep 2
+echo $green"jika aku sudah pergi"
+sleep 2
+echo $white"bakal susah diajak balikan.."
+sleep 3
+fi
+
+if [ $pil = '7' ]
+then
+clear
+echo
+toilet -f future Bucin by. LiaCans | lolcat
+echo
+echo
+sleep 0.1
+echo $red"kalau menunggu itu membosankan?"
+sleep 1
+echo $purple"lalu? kenapa kau suruh aku menunggu?"
+sleep 3
+fi
+
+if [ $pil = '8' ]
+then
+clear
+echo
+toilet -f future Bucin by. RelGanz2 | lolcat
+echo
+echo
+sleep 0.1
+echo $yellow"Tidak peduli apa yang telah terjadi"
+sleep 2
+echo $white"Tidak peduli apa yang telah kamu lakukan"
+sleep 2
+echo $yellow"Aku akan selalu mencintaimu"
+sleep 2
+echo $whitw"Aku bersumpah untuk itu"
+sleep 4
+clear
+echo
+toilet -f future bucin by. RelGanz2 | lolcat
+echo
+echo
+sleep 0.1
+echo $indigo"Aku melihat kesempurnaan pada dirimu, lalu aku jatuh cinta padamu"
+sleep 2
+echo $indigo"Kemudian, aku melihat ketidaksempurnaanmu, akupun semakin jatuh cinta padamu"
+sleep 4
+clear
+echo
+toilet -f future Bucin by. RelGanz2 | lolcat
+echo
+echo
+sleep 0.1
+echo $red"Aku mencintai mu, dan aku akan tetap mencintaimu sampai mati"
+sleep 2
+echo $white"Jika ada kehidupan lagi setelah itu, maka aku akan mencintai mu lagi :)"
+sleep 3
+fi
+
+if [ $pil = '9' ]
+then
+clear
+echo
+toilet -f future bucin by. TeamGanz2 | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Tidak ada yang setia di dunia ini bahkan bayanganmu sendiripun akan pergi menghilang dalam kegelapan"
+sleep 2
+echo
+echo $indigo"Apa kah aku harus mengganti namaku menjadi aamiin,agar selalu kau sebut dalam doa mu:)"
+sleep 2
+echo
+echo $green"Bukan kebahagian yg membuat kita bersyukur,tetapi bersyukurlah yg membuat kita bahagia:)"
+sleep 3
+fi
+
+if [ $pil = '10' ]
+then
+clear
+echo
+figlet -f future bucin by. Team Ganz3 | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Cinta itu dari hati yang terikat di dalam fikiran. Jika hati sudah saling bisa memahami dan meyakininya itu baru dnamakan cinta"
+echo
+sleep 2
+echo $red"Jika kamu mencintai dua orang dalam satu waktu pilihlah yang pertama karena jika kamu benar-benar  mencintai yang pertama tidak akan pernah ada yg kedua"
+sleep 3
+fi
+
+#thanks udh gunain toolsnya
+#belajar coding
+#recode izin 0882-1098-2908
